dffe8708e2dc6926e7b1ea93ee4bb3021c6f6513
黄锴 <huangkai1@corp.netease.com>
2020-09-05 12:23:34 +0800

    fix: 基于基础dom

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const render = () => {};
+const createElement = (tag, attrs, ...children) => {
+    const el = document.createElement(tag);
+    for(attr in attrs) {
+        el.setAttribute(attr, attrs[attr]);
+    }
+    children.forEach(child => {
+        el.appendChild(child);
+    });
+    return el;
+};
+
+const render = (dom) => {
+    document.body.appendChild(dom);
+};
+
+
 render(<div>
-    <div>1213</div>
-    <p>1232</p>
+    <div></div>
+    <p></p>
 </div>);
